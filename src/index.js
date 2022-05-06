141971ec24125a6076a01a0dc63225b334dbf086
odzen <jsebastian.va@gmail.com>
2022-05-06 00:18:39 -0500

    Adding the body parser to recive data in JSON format

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ const express = require('express');
 
 const app = express();
 
+const bodyParser = require("body-parser");
 // Routes for version 1
 const v1PictureRouter = require('./v1/routes/astronomyRoutes');
 
 const PORT = process.env.PORT || 3000; 
 
-
+// Middlewares
+app.use(bodyParser.json());
 app.use('/api/v1/pictures', v1PictureRouter);
 
 
