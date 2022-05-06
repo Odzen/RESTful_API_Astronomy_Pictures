ab583c548989992c6b6ad4fa1ea470cf660b8b8c
odzen <jsebastian.va@gmail.com>
2022-05-05 19:51:15 -0500

    Markup the service JS file, structuring

---
+++ src/services/astronomyService.js
@@ -0,0 +1,27 @@
+const getAllPicturess = () => {
+    return;
+  };
+  
+  const getOnePictures = () => {
+    return;
+  };
+  
+  const createNewPictures = () => {
+    return;
+  };
+  
+  const updateOnePictures = () => {
+    return;
+  };
+  
+  const deleteOnePictures = () => {
+    return;
+  };
+  
+  module.exports = {
+    getAllPicturess,
+    getOnePictures,
+    createNewPictures,
+    updateOnePictures,
+    deleteOnePictures,
+  };
