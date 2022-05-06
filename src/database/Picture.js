ee11d16947e496e0541dd5960d6a0fab691d9b48
odzen <jsebastian.va@gmail.com>
2022-05-05 21:18:45 -0500

    Seeting up the Picture.js

--- src/database/Picture.js
+++ src/database/Picture.js
@@ -1,7 +1,13 @@
 // Data Access Layer - here we work with our MongoDB.
 // And send the data over to the service layer
 
+const DB = require("./seeds.js");
+
+const getAllPictures = () => {
+  return DB.Pictures;
+};
 
 
 // Exporting methods for certain database operations
 // used by the Service Layer.
+module.exports = { getAllPictures };
