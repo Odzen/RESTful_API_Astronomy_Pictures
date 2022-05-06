e9f9b20b2a8fb2b1af1e562567fc235250d5c8ba
odzen <jsebastian.va@gmail.com>
2022-05-05 23:48:24 -0500

    Populating data MongoDB is working

--- src/database/Picture.js
+++ src/database/Picture.js
@@ -1,7 +1,41 @@
 // Data Access Layer - here we work with our MongoDB.
 // And send the data over to the service layer
 
-const DB = require("./seeds.js");
+const DB = require('./seeds.js');
+const mongoose = require('mongoose');
+
+// Connecting to mongoose
+// Import the model
+const Picture = require('./models/picture');
+
+async function main() {
+    try{
+        await mongoose.connect('mongodb://localhost:27017/astronomy');
+        console.log("MONGO CONNECTION OPEN!");  
+    }
+    catch(err){
+        console.log("ERROR TRYING TO CONNECT to MONGODB :(");
+        console.log(err);
+    }
+}
+
+main();
+
+// Get initial images and save them into the DB
+const getSaveAllInitialPictures = async () => {
+    const Data =  await DB.getAllPicturesNasa();
+    let count = 0;
+    for (element of Data){
+        const {explanation, hdurl, title, url} =  element;
+        if(explanation && hdurl && title && url){
+            const newPicture = new Picture(element);
+            await newPicture.save();
+            count += 1;
+        }
+    }
+    console.log(`Populate the DB with ${count} pictures from the NASA API`);
+};
+getSaveAllInitialPictures();
 
 // Working with database to make the database consistent
 const getAllPictures = async () => {
@@ -9,10 +43,7 @@ const getAllPictures = async () => {
     return Data;
 };
 
-const getAllInitialPictures = async () => {
-    const Data =  await DB.getAllPicturesNasa();
-    return Data;
-};
+
 
 //console.log(getAllPictures());
 
---
+++ src/database/models/picture.js
@@ -0,0 +1,33 @@
+// Schema for the pictures collection
+
+const mongoose = require('mongoose');
+
+const pictureSchema = new mongoose.Schema({
+    explanation: {
+        type : String,
+        required : true,
+        maxLength : 2000
+    },
+    hdurl : {
+        type : String,
+        required : true,
+        lowercase : true
+    },
+    title : {
+        type : String,
+        required : true,
+        lowercase : true
+    },
+    url :{
+        type : String,
+        required : true,
+        lowercase : true
+
+    }
+});
+
+// Compile the model
+const Picture = mongoose.model('Picture', pictureSchema);
+
+// Export the model from the file
+module.exports = Picture;
