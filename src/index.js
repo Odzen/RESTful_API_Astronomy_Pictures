5e8baf53ebc6d75c9ef54b67b053f4095847ad21
odzen <jsebastian.va@gmail.com>
2022-05-06 22:56:03 -0500

    First steps setting up a doc endpoint to show pretty documentation

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ const express = require('express');
 
 const app = express();
 
-const bodyParser = require("body-parser");
-// Routes for version 1
+const bodyParser = require('body-parser');
+
 const v1PictureRouter = require('./v1/routes/astronomyRoutes');
+
+const V1SwaggerDocs = require('./v1/swagger');
+ 
 
 const PORT = process.env.PORT || 3000; 
 
@@ -17,5 +20,6 @@ app.use('/api/v1/pictures', v1PictureRouter);
 
 
 app.listen(PORT, () => { 
-    console.log(`API is listening on port ${PORT} - Wait! Data is comming...`); 
+    console.log(`API is listening on port ${PORT} - Wait! Data is comming...`);
+    V1SwaggerDocs.swaggerDocs(app, PORT);
 });
