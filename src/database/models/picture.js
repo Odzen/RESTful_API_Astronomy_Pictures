fec3bc91903143f43558230834eb3f8691d376e0
odzen <jsebastian.va@gmail.com>
2022-05-07 00:21:21 -0500

    Doc for get all pictures route, have an issue with schema

--- src/database/models/picture.js
+++ src/database/models/picture.js
@@ -1,5 +1,39 @@
 // Schema for the pictures collection
 
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     Picture:
+ *       type: object
+ *       properties:
+ *         _id:
+ *           type: string
+ *           example: 6275ef7b57812472addd5e56
+ *         explanation:
+ *           type: string
+ *           example: Would you like to see a total eclipse of the Sun?  If so, do any friends or relatives live near the path of next summer's eclipse?  If yes again, then...
+ *         hdurl:
+ *           type: string
+ *           example: https://apod.nasa.gov/apod/image/1608/tse2017usa_espenak_1421.jpg
+ *         title:
+ *           type: string
+ *           example: Map of Total Solar Eclipse Path in 2017 August
+ *         url:
+ *           type: string
+ *           example: https://apod.nasa.gov/apod/image/1608/tse2017usa_espenak_1080.jpg
+ *         createdAt:
+ *           type: string
+ *           example: 2022-05-07T04:03:07.570Z
+ *         updatedAt:
+ *           type: string
+ *           example: 2022-05-07T04:03:07.570Z
+ *         __v:
+ *           type: number
+ *           example: 0
+ */
+
+
 const mongoose = require('mongoose');
 
 // Plugging for pagination
--- src/v1/routes/astronomyRoutes.js
+++ src/v1/routes/astronomyRoutes.js
@@ -1,5 +1,70 @@
 // Route Layer - V1
 
+/**
+ * @openapi
+ * /api/v1/pictures:
+ *   get:
+ *     tags:
+ *       - Pictures
+ *     parameters:
+ *       - in: query
+ *         name: title
+ *         schema:
+ *           type: string
+ *         description: Returns a list of matches where the query appears in the picture's titles from the database
+ *       - in: query
+ *         name: limit
+ *         schema:
+ *           type: number
+ *         description: The limit of responses
+ *       - in: query
+ *         name: explanation
+ *         schema:
+ *           type: string
+ *         description: Returns a list of matches where the query appears in the picture's explanations from the database
+ *       - in: query
+ *         name: url
+ *         schema:
+ *           type: string
+ *         description: The url of a picture
+ *       - in: query
+ *         name: hurl
+ *         schema:
+ *           type: string
+ *         description: The url of a picture in high definition
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Picture'
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   type: object
+ *                   
+ *       5XX:
+ *         description: FAILED
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status: 
+ *                   type: string
+ *                   example: FAILED
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     error:
+ *                       type: string 
+ *                       example: "Some error message"
+ */
+
 const express = require('express');
 const router = express.Router();
 const pictureController = require('../../controllers/astronomyController');
