2905dbf15bb4f04d6566cc3a43c1f8e7a83a9455
odzen <jsebastian.va@gmail.com>
2022-05-07 23:03:52 -0500

    Add JSON message for not valid routes

--- src/index.js
+++ src/index.js
@@ -32,11 +32,36 @@ const app = express();
 const HOST = process.env.HOST || '0.0.0.0';
 const PORT = process.env.PORT || 3000; 
 
-// Middlewares
+
+// Middlewares for VALID routes /api/v1/pictures -- /api/v1/docs
 app.use(bodyParser.json());
 app.use('/api/v1/pictures', v1PictureRouter); // routes
 app.use('/api/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerJSDoc(swaggerSpec)));
 
+
+// For OTHER routes different from the api/v1 which is the valid one
+app.get("/", (req, res) => {
+    res.send({ status: "The route is not valid, use the following to consume the data",
+               validRoutes: {
+                   endpointToInteractWithPictures : "/api/v1/pictures",
+                   endpointForDocumentation : "/api/v1/docs"
+               } });
+});
+app.get("/api", (req, res) => {
+    res.send({ status: "The route is not valid, use the following to consume the data",
+    validRoutes: {
+        endpointToInteractWithPictures : "/api/v1/pictures",
+        endpointForDocumentation : "/api/v1/docs"
+    } });
+});
+
+app.get("/api/v1", (req, res) => {
+    res.send({ status: "The route is not valid, use the following to consume the data",
+    validRoutes: {
+        endpointToInteractWithPictures : "/api/v1/pictures",
+        endpointForDocumentation : "/api/v1/docs"
+    } });
+});
 
 app.listen(PORT,HOST, () => { 
     console.log(`API is listening on port ${PORT} - Wait! Data is comming...`);
